da4e948533a79effdd2547a82fce370975fabf5f
John Nolan <jjnolan@virginmedia.com>
2016-11-04 20:21:53 +0000

    -- spJournalIndexGet -- Journal Index logic

--- Ammo.Domain/Repositories/Concrete/JournalIndexRepository.cs
+++ Ammo.Domain/Repositories/Concrete/JournalIndexRepository.cs
@@ -1,18 +1,43 @@
 ﻿using Ammo.Domain.Repositories.Abstract;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Ammo.Domain.Entities;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+using static Dapper.SqlMapper;
 
 namespace Ammo.Domain.Repositories.Concrete
 {
     public class JournalIndexRepository : BaseRepository, IJournalIndexRepository
     {
+        /// <summary>
+        /// Retrieves the Journal Index page data 
+        ///     -- Bullets 
+        ///     -- Tags
+        ///     -- Bookmarks
+        /// </summary>
+        /// <param name="JournalId"></param>
+        /// <returns></returns>
         public JournalIndex Get(int JournalId)
         {
-            throw new NotImplementedException();
+            JournalIndex index;
+
+            using (var connection = new SqlConnection(base.ConnectionString))
+            {
+                using (GridReader reader = connection.QueryMultiple("spJournalIndexGet", new { @JOURNALID = JournalId }, null, null, CommandType.StoredProcedure))
+                {
+                    index = reader.Read<JournalIndex>().Single();
+
+                    index.Bookmarks = reader.Read<JournalBookmark>().ToList();
+
+                    index.Bullets = reader.Read<BulletCollection>().Single();
+                    index.Bullets.Bullets = reader.Read<Bullet>().ToList();
+
+                    index.Tags = reader.Read<JournalTag>().ToList();
+                }
+            }
+
+            return index;
         }
     }
 }
